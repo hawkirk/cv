31b201d1711b2933ad7928d411e2fab306f07775
baileywickham <baileywickham@gmail.com>
2020-09-19 00:20:22 -0700

    update files

--- toPDF.sh
+++ toPDF.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-phantomjs toPDF.js 'http://localhost:4200' public/Lucas_Clemente.pdf A4
+python3 -m http.server 4200 &
+phantomjs-2.1.1-linux-x86_64/bin/phantomjs toPDF.js 'http://localhost:4200' public/Lucas_Clemente.pdf A4
