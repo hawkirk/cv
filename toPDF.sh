99b74e0b951dccd3d3982a2525b43e9eb833e342
baileywickham <baileywickham@gmail.com>
2020-09-20 18:20:48 -0700

    update to use yarn

--- toPDF.sh
+++ toPDF.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python3 -m http.server 4200 &
-phantomjs-2.1.1-linux-x86_64/bin/phantomjs toPDF.js 'http://localhost:4200' public/Lucas_Clemente.pdf A4
+phantomjs-2.1.1-linux-x86_64/bin/phantomjs toPDF.js 'http://localhost:4200' public/wickham.pdf A4
